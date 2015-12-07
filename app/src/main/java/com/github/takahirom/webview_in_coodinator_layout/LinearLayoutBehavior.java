c893fa66ab35f3fb9a521026292b2053bb92c751
takahirom <takam.dev@gmail.com>
2015-12-08 02:09:26 +0900

    Change behavior transition logic

--- app/src/main/java/com/github/takahirom/webview_in_coodinator_layout/LinearLayoutBehavior.java
+++ app/src/main/java/com/github/takahirom/webview_in_coodinator_layout/LinearLayoutBehavior.java
@@ -20,23 +20,22 @@ import android.content.Context;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.view.ViewCompat;
-import android.support.v4.view.ViewParentCompat;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
 public class LinearLayoutBehavior extends CoordinatorLayout.Behavior<LinearLayout> {
 
+    private int defaultDependencyTop = -1;
+
     public LinearLayoutBehavior(Context context, AttributeSet attrs) {
-        super();
+        super(context, attrs);
     }
 
     @Override
     public boolean onStartNestedScroll(final CoordinatorLayout coordinatorLayout, final LinearLayout child,
                                        final View directTargetChild, final View target, final int nestedScrollAxes) {
-        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL
-                || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
     }
 
     @Override
@@ -44,24 +43,14 @@ public class LinearLayoutBehavior extends CoordinatorLayout.Behavior<LinearLayou
         return dependency instanceof AppBarLayout;
     }
 
+
     @Override
     public boolean onDependentViewChanged(CoordinatorLayout parent, LinearLayout child, View dependency) {
-        final CoordinatorLayout.Behavior behavior =
-                ((CoordinatorLayout.LayoutParams) dependency.getLayoutParams()).getBehavior();
-
-        final int topAndBottomOffset = ((AppBarLayout.Behavior) behavior).getTopAndBottomOffset();
-        Log.d("LinearLayoutBehavior", "topAndBottomOffset:" + topAndBottomOffset);
-        Log.d("LinearLayoutBehavior", "ViewCompat.getTranslationY(dependency):" + ViewCompat.getTranslationY(dependency));
-        child.setTranslationY(-topAndBottomOffset);
+        if (defaultDependencyTop == -1) {
+            defaultDependencyTop = dependency.getTop();
+        }
+        child.setTranslationY(-dependency.getTop() + defaultDependencyTop);
         return true;
-//        final float toY = child.getTranslationY() - dy - mLayoutHeight;
-//        if (0 < toY) {
-//            child.setTranslationY(mLayoutHeight);
-//        } else if (-mLayoutHeight > toY) {
-//            child.setTranslationY(0);
-//        } else {
-//            child.setTranslationY(mLayoutHeight + toY);
-//        }
     }
 
 }
